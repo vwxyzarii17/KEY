e87ad9b1de01a2956dd8d7baba9438955b172934
vwxyzarii17 <markocop170104@gmail.com>
2026-09-18 10:10:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def home():
     response_class=PlainTextResponse
 )
 def clikerads_verify():
-    return "ISI_KODE_VERIFIKASI_DARI_CLIKER_ADS"
+    return "verify_a0bd63d949f31dff8cf54ba9895456e87f799577"
 
 
 @app.get("/generate", response_class=PlainTextResponse)
